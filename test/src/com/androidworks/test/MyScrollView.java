8fab6f05766ee3c8900a08563768fcdd6d6aa403
smineyev@androidworks.com <smineyev@androidworks.com>
2010-09-22 23:49:55 -0700

    test

---
+++ test/src/com/androidworks/test/MyScrollView.java
@@ -0,0 +1,22 @@
+package com.androidworks.test;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+public class MyScrollView extends ScrollView {
+
+	public MyScrollView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	public MyScrollView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	public MyScrollView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
